8d0a71f9ec6eebd5dd7c4149af15c66eb8288460
KeRaBiN <kuzmat278@gmail.com>
2023-05-17 21:32:38 +0300

    updated pages

--- includes/header.php
+++ includes/header.php
@@ -2,21 +2,21 @@
 session_start();
 ?>
 
-<link rel="stylesheet" href="../assets/css/header.css">
+<link rel="stylesheet" href="/assets/css/header.css">
     <header>
         <div class="header-title">
-            <h1><a href="../mainpage.php">FindMyTeam</a></h1>
+            <h1><a href="/mainpage.php">FindMyTeam</a></h1>
         </div>
         <nav>
             <div class="menu-wrapper">
                 <ul class="left-menu">
-                    <li><a href="../mainpage.php">Пошук товариша</a></li>
+                    <li><a href="/mainpage.php">Пошук товариша</a></li>
                     <li><a href="#">Контакти</a></li>
-                    <li><a href="../aboutus.php">Про нас</a></li>
+                    <li><a href="/aboutus.php">Про нас</a></li>
                 </ul>
                 <ul class="right-menu">
-                    <li class="auth-button"><?php if($_SESSION['user']) {?><a href="../account.php.php">Профіль</a></li><?php } else?> <a href="../auth.php">Увійти</a></li>
-                    <li class="auth-button"><?php if($_SESSION['user']) {?><a href="../includes/logout.php">Вихід</a></li><?php } else?> <a href="../register.php">Зареєструватися</a></li>
+                    <li class="auth-button"><?php if($_SESSION['user']) {?><a href="/account.php.php">Профіль</a></li><?php } else?> <a href="/auth.php">Увійти</a></li>
+                    <li class="auth-button"><?php if($_SESSION['user']) {?><a href="/includes/logout.php">Вихід</a></li><?php } else?> <a href="/register.php">Зареєструватися</a></li>
                 </ul>
             </div>
         </nav>
