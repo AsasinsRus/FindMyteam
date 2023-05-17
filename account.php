551bbfb4477a5a8bddaf9114c9ff5bd166a75938
Yurii <minewar6@gmail.com>
2023-05-17 15:48:07 +0200

    account added

---
+++ account.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Steam Profile</title>
+    <link rel="stylesheet" href="assets/css/account.css">
+</head>
+<body>
+<div class="profile">
+    <div class="profile-header">
+        <img class="avatar" src="assets/pics/avatar.png" alt="Avatar">
+        <h1 class="username">Username</h1>
+    </div>
+    <div class="profile-body">
+        <div class="profile-info">
+            <h2>About Me</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu eleifend ligula. Morbi auctor dapibus tempor. Sed consequat felis id metus rhoncus, ac feugiat nisl iaculis. In a arcu sed nunc semper aliquet.</p>
+        </div>
+        <div class="game-list">
+            <h2>My Games</h2>
+            <ul>
+                <li>Game 1</li>
+                <li>Game 2</li>
+                <li>Game 3</li>
+                <li>Game 4</li>
+            </ul>
+        </div>
+    </div>
+    <div class="profile-footer">
+        <p>&copy; 2023 FindMyTeam. All rights reserved.</p>
+    </div>
+</div>
+</body>
+</html>
